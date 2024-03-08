7b9a8b2d662673194ee84ed7a4966c245b62e969
ankitsinsinwar <theankitfaujdar@gmail.com>
2024-03-09 00:23:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   const [data,setdata] = useState({})
   const [location,setlocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=d4279a0f7a6b8fa80e11691881c094db`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=d4279a0f7a6b8fa80e11691881c094db`
 
   const searchLocation = (e)=>{
       axios.get(url).then((Res)=>{
         setdata(Res.data)
-        console.log(Res.data);
+        //console.log(Res.data);
       }) 
     }
     
@@ -29,7 +29,7 @@ function App() {
           {data.main ?   <p>{data.name},<span>{data.sys.country}</span></p> : null}
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
             
           </div>
           <div className="discription">
@@ -39,7 +39,7 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°F</p> :null}
+            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> :null}
             
             <p>Feels Like</p>
           </div>
@@ -49,7 +49,7 @@ function App() {
             <p>Humidity</p>
           </div>
           <div className="wind">
-            {data.wind ? <p className="bold">{data.wind.speed.toFixed()}MPH</p> : null}
+            {data.wind ? <p className="bold">{data.wind.speed.toFixed()} KMPH</p> : null}
             
             <p>Wind</p>
           </div>
